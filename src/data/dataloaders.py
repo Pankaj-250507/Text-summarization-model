c4d7723f01f57ea90b52d66bee567956233fbae8
PANKAJ SHARMA <pankajofficial2507ps@gmail.com>
2026-09-06 17:35:01 +0530

    Implemented changes in Dataloder and resolved conflicts

--- src/data/dataloaders.py
+++ src/data/dataloaders.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
+from src.model.config import data_config
+
 
 
 class CustomDataset(Dataset):
@@ -45,36 +47,40 @@ class CustomDataset(Dataset):
 
     
 class get_dataloader:
-    def __init__(self,train_path:list ,val_path :list ,block_size :int, batch_size :int , num_workers:int):
+    def __init__(self, data_config, train_path, val_path):
         super().__init__()
-        self.train_path = train_path
-        self.val_path = val_path
-        self.block_size = block_size
-        self.batch_size = batch_size
-        self.num_workers = num_workers
+        assert train_path is not None, "train_path cannot be None"
+        assert val_path is not None, "val_path cannot be None"
+        self.train = CustomDataset(train_path)
+        self.val = CustomDataset(val_path)
+
+        assert data_config is not None, "data_config cannot be None"
+        self.data_config = data_config
 
         
     def train_dataloader(self):
         dataset = CustomDataset(self.train_path,self.block_size)
         return DataLoader(
-            dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            pin_memory=True,
-            num_workers=self.num_workers,
-            drop_last=True
+            self.train,
+            batch_size=self.data_config.batch_size,
+            num_workers=self.data_config.num_workers,
+            prefetch_factor=self.data_config.prefetch_factor,
+            pin_memory=self.data_config.pin_memory,
+            in_order=self.data_config.in_order,
+            shuffle = True
         )
 
     def val_dataloader(self):
-        dataset = CustomDataset(self.val_path,self.block_size)
         return DataLoader(
-            dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            pin_memory=True,
-            num_workers=self.num_workers,
-            drop_last=False
+            self.val,
+            batch_size=self.data_config.batch_size,
+            num_workers=self.data_config.num_workers,
+            prefetch_factor=self.data_config.prefetch_factor,
+            pin_memory=self.data_config.pin_memory,
+            in_order=self.data_config.in_order,
+            shuffle = False
         )
+
 
 
 # for testing purpose only
